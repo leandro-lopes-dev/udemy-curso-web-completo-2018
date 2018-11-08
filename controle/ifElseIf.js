797f1ab123249f93437179e573428f8046dee034
Leandro Lopes <macuser@iMac.local>
2018-11-07 23:41:09 -0200

    exercicios de fixacao

--- controle/ifElseIf.js
+++ controle/ifElseIf.js
@@ -1,39 +1,27 @@
-Number.prototype.entre = function(inicio, fim){
-
-    return this >= inicio && this <= fim
+Number.prototype.entre = function (inicio , fim){
+    return this >= inicio && this <= fim;
 }
 
-const imprimirResultado = function (nota){
-    if(nota.entre(9,10)){
+const imprimirResultado = function(nota){
+    if(nota.entre(9, 10)){
         console.log('Quadro de Honra')
-    }else if (nota.entre(7, 8.99)){
-        console.log('Aprovado')
-    }else if(nota.entre(4, 6.99)){
-        console.log('Recuperação')
-    }else if(nota.entre(0, 3.99)){
+    }else if(nota.entre(6, 8)){
+        console.log('Aluno normal')
+    }else if(nota.entre(4, 5.99)){
+        console.log('Aluno em recuperação')
+    }else if(nota.entre(1, 3.99)){
         console.log('Reprovado')
     }else{
-        console.log('Nova Inválida')
+        console.log('Nota Inválida')
     }
-    console.log('fim')
 }
 
-imprimirResultado(10)
-imprimirResultado(8.9)
-imprimirResultado(6.55)
-imprimirResultado(2.3)
-imprimirResultado(-1)
-imprimirResultado(11)
+console.log('fim');
 
-
-
-
-
-
-
-
-
-
-
-
-
+imprimirResultado(10);
+imprimirResultado(8);
+imprimirResultado(7);
+imprimirResultado(2);
+imprimirResultado(2.99);
+imprimirResultado(0);
+imprimirResultado(5);
---
+++ controle/meusTestes.js
@@ -0,0 +1,18 @@
+function meusTestes(exercitando){
+    if(exercitando >=10){
+        console.log('OK!')
+    }else{
+        console.log('Não!')
+    }
+}
+meusTestes(9)
+
+
+function teste2(num){
+    if(num>7);{
+        console.log(num)
+    }
+}
+
+teste2(6);
+teste2(8);
