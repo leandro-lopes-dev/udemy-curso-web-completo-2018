9144b0764eb4da139eaf2974899d01657cd9a2b7
Leandro Lopes <leandro.lopes.dev@gmail.com>
2018-11-09 13:45:09 -0200

    entendendo Objeto

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,8 @@
+// Objeto é uma coleção dinâmica de chaves / valor
+const produto = new Object
+produto.nome = 'Cadeira';
+produto ['marca do produto'] = 'Generica';
+produto.preco = 220;
+
+console.log(produto);
+
